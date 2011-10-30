b0557fdb68ad0fe571dd947e20c6dbe39a1d8b3b
Alexandru Ghiura <ghalex@gmail.com>
2011-10-30 11:48:05 +0000

    Add build code

---
+++ build/core/files.js
@@ -0,0 +1,64 @@
+var files = [
+    
+    'core/Core.js',
+    
+    // Constants 
+    'constants/VerticalAlign.js',
+    'constants/HorizontalAlign.js',
+    'constants/CollectionEventKind.js',
+    'constants/PopUpSnap.js',
+    
+    // Events
+    'events/Event.js',
+    'events/CollectionEvent.js',
+    'events/ChildEvent.js',
+    'events/DataEvent.js',
+    'events/ListEvent.js',
+    'events/MenuEvent.js',
+    
+    // Core
+    'core/Events.js',
+    'core/Element.js',
+    'core/Iterator.js',
+    'core/ArrayCollection.js',
+    'core/Function.js',
+    'core/ChildList.js',
+    'core/BackboneModel.js',
+    
+    // Managers 
+    'managers/PopUpManager.js',
+    'managers/ThemeManager.js',
+    
+    // HTML
+    'html/Table.js',
+    
+    // UI
+    'ui/Object.js',
+    'ui/Component.js',
+    'ui/controls/Modal.js',
+    'ui/controls/Button.js',
+    'ui/controls/CheckBox.js',
+    'ui/controls/TextInput.js',
+    'ui/controls/Image.js',
+    'ui/controls/Label.js',
+    
+    // Containers
+    'ui/containers/Container.js',
+    'ui/containers/View.js',
+    'ui/containers/ViewStack.js',
+    'ui/containers/HTMLBox.js',
+    'ui/containers/ScrollBox.js',
+    'ui/containers/CellBox.js',
+    'ui/containers/RootBox.js',
+    'ui/containers/PopUp.js',
+    'ui/containers/HBox.js',
+    'ui/containers/VBox.js',
+    'ui/renderers/ItemRenderer.js',
+    'ui/renderers/ListItemRenderer.js',
+    'ui/renderers/MenuItemRenderer.js',
+    'ui/containers/ListBase.js',
+    'ui/containers/List.js',
+    'ui/controls/Menu.js'
+];
+
+exports.all = files;
---
+++ build/index.js
@@ -0,0 +1,10 @@
+
+require('./libs/mootools').apply(GLOBAL);
+
+var fs = require('fs');
+var files = require('./core/files');
+var rootDirectory = 'src/phi';
+
+
+//var filedata = fs.readFileSync('src/phi/ui/Object.js', 'utf-8');
+//console.log(files.all);
